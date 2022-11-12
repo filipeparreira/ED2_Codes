3df75b4cf7c6af2a893445b7b4c49ffba1b4fb99
Filipe Augusto <filipeaugusto@alunos.utfpr.edu.br>
2022-11-11 22:01:50 -0300

    Atividade 3

---
+++ AT3/at3.py
@@ -0,0 +1,9 @@
+
+def ordena(arq_in, arq_out):
+    arq_in = open(arq_in, 'r')
+    arq_out = open(arq_out, 'w')
+
+if __name__ == '__main__':
+    arq_in = 'input1.txt'
+    arq_out = 'saida_teste1.txt'
+
