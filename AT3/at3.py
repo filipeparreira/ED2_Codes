3378b8c42c99bfb51c5bcfdb7f2e919f2e04f4f8
Filipe Augusto <filipeaugusto@alunos.utfpr.edu.br>
2022-11-12 18:56:50 -0300

    Atividade 3

--- AT3/at3.py
+++ AT3/at3.py
@@ -1,9 +1,43 @@
 
-def ordena(arq_in, arq_out):
-    arq_in = open(arq_in, 'r')
-    arq_out = open(arq_out, 'w')
+def armazena(arquivo_in):
+    arquivo = open(arquivo_in, 'r')
+    linhas = arquivo.readlines()
+    
+    #Armazenando as informações do cabeçalho
+    cabecalho = linhas[0].split('=')
+    
+    #Quantidade
+    cabecalho_aux = cabecalho[3].split(' ')
+    qntd = int(cabecalho_aux[0])
+
+    #Tipo de ordenação
+    cabecalho_aux = cabecalho[4].split(' ')
+    tipo_ord = cabecalho_aux[0]
+
+    #Ordem da ordenação
+    cabecalho_aux = cabecalho[5].split(' ')
+    ordem = cabecalho_aux[0]
+
+    #Armazendando as keys
+    count = 1
+    keys = []
+    
+    while count < len(linhas):
+        conteudo = linhas[count].split('|')
+        keys.append(conteudo[0])
+        count += 1
+
+    arquivo.close()
+
+    return qntd, tipo_ord, ordem, keys
+
 
 if __name__ == '__main__':
-    arq_in = 'input1.txt'
-    arq_out = 'saida_teste1.txt'
+    #Recebendo o arquivo:
+    arquivo_in = 'input1.txt'
+    arquivo_saida = 'saida_teste1.txt'
+    
+    #Armazena quantidade, tipo_ord, ordem, keys
+    quantidade, tipo_ord, ordem, keys = armazena(arquivo_in)
 
+    
---
+++ AT3/teste_sort_methods.py
@@ -0,0 +1,29 @@
+from sorting import quick, merge, heap
+'''from sorting_techniques import pysort
+
+#Criação do objeto que representa o pysort
+object_sort = pysort.Sorting()'''
+
+lista_pessoas = []
+
+pessoas = {'key' : 30, 'nome' : 'Filipe', 'idade' : 21, 'sexo' : 'Masculino'}
+lista_pessoas.append(pessoas)
+pessoas = {'key' : 20, 'nome' : 'Almeida', 'idade' : 20, 'sexo' : 'Masculino'}
+lista_pessoas.append(pessoas)
+pessoas = {'key' : 10, 'nome' : 'Bento', 'idade' : 23, 'sexo' : 'Masculino'}
+lista_pessoas.append(pessoas)
+
+
+print(lista_pessoas[0]['nome'])
+merge(lista_pessoas)
+print(lista_pessoas[0]['nome'])
+
+'''quick = object_sort.quickSort(arr.copy(), 0, len(arr) - 1)
+print(quick)
+merge = object_sort.mergeSort(arr.copy())
+print(merge)
+heap = object_sort.heapSort(arr.copy())
+print(heap)
+insertion = object_sort.insertionSort(arr.copy())
+print(insertion)
+'''
