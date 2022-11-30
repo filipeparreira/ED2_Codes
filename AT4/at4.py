4712558703af20cd00d3a4df4115140d5acf4b86
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2022-11-30 17:23:56 -0300

    Arquivo Python da atividade

---
+++ AT4/at4.py
@@ -0,0 +1,17 @@
+'''
+    ** Itens que vão conter em cada registro:
+        - Ano: o ano que a música foi lançada [5]
+        - Duração: a duração em minutos e segundos [6] 
+        - Título: o título da música [31]
+        - Artista: o artista que gravou a música [21]
+        - Gênero: o gênero da música [12]
+        - Idioma: o idioma da canção [12]
+    
+    **Exigências do programa:
+        - O programa vai receber três arquivos como parâmetro, um arquivo contendo as músicas,
+        um arquivo de consultas e um arquivo de saída.
+        - Será necessario implementar um índice primário, para que o secundário funcione
+        - As funções que vão ser necessárias são: 
+            -  Criar, carregar e pesquisar (Tanto no indice primario quanto no indice secundario)
+'''
+
