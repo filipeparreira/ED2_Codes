3094f7999dad9643f8c13d87af7812c3c1dc6808
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2022-11-30 17:33:13 -0300

    Funções principais para Idx Primario
    Comentário das funções principais

--- AT4/at4.py
+++ AT4/at4.py
@@ -12,6 +12,14 @@
         um arquivo de consultas e um arquivo de saída.
         - Será necessario implementar um índice primário, para que o secundário funcione
         - As funções que vão ser necessárias são: 
-            -  Criar, carregar e pesquisar (Tanto no indice primario quanto no indice secundario)
+            - Criar, carregar e pesquisar (Tanto no indice primario quanto no indice secundario)
+            - E funções auxiliares (Ler_registro_com_RRN, ordenarIndx, criar_RRN)
 '''
+# Para o funcionamento do indice primario
+    # Ler um registro com base no RRN
+    # Ordenar o arquivo de indices 
+    # Gerar chave canonica
+    # Criar tabela de indices, onde cada indice é uma tupla (RRN, cc)
+    # Carregar as informações dos registros para a tabela de indices 
+    # Pesquisar baseado na chave canonica
 
